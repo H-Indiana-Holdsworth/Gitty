d714bbbf3dc4a925d14b702ce4b7d1bdf5943cbc
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-03-31 11:57:27 -0700

    getGithubProfile

--- __tests__/github.test.js
+++ __tests__/github.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/github');
+
 describe('Gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -18,5 +20,13 @@ describe('Gitty routes', () => {
     expect(req.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
+  });
+
+  it('should login and redirect to posts', async () => {
+    const req = await request
+      .agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      // check for res.redirects to see if it directed us back to the posts route
+      .redirects(1);
   });
 });
